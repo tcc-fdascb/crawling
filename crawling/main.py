a511475912032c50102b2f0e88d807f6e03f045f
Flávio Dutra <flavio.adore@gmail.com>
2020-09-02 12:06:03 -0300

    Revert "Refatoração do código com implementação de threads e exceptions"
    This reverts commit aee3f2a72dfb6db2a8190425dfc0e551a26857af.

--- crawling/main.py
+++ crawling/main.py
@@ -1,122 +1,66 @@
 import requests
-from threading import Thread
 import pandas as pd
 import urllib.robotparser
-from datetime import datetime as dt
-import sys
 
 from crawling.recommendations import *
 from crawling.occurrences import Occurrences
 
 
-CSV_FILE = 'data/cities-abc.csv'
-
 # Inicializa uma estância para lista de ocorrências
 occurrences = Occurrences()
 
 
-def csv_file_to_dict(file):
-    try:
-        data = pd.read_csv(file)
-        data = data.to_dict(orient='index')
-
-        return data
-
-    except Exception:
-        raise
+# Faz leitura do arquivo CSV e converte para dict
+cities = pd.read_csv('data/cities-sa.csv')
+cities = cities.to_dict(orient='index')
+print(cities)
 
 
-class ValidateCity(Thread):
-    """
-    Conjunto de métodos para aplicação de Threads e avaliações para os sítios eletrônicos.
-    """
+# Confere o arquivo robots.txt dos sites e verifica condições de Dissalow adicionando duas keys novas ao dict cities:
+#   - has_robotstxt (True/False) - responde a pergunta se tem ou não o arquivo robots.txt
+#   - can_crawling (True/False) - responde a pergunta se pode ou não fazer crawling
+for city in cities:
+    cities[city]['has_robotstxt'] = False
+    cities[city]['can_crawling'] = False
 
-    def __init__(self, city):
-        Thread.__init__(self)
-        self.city = city
-        self.sourcecode = self.get_sourcecode()
+    city_url = cities[city]['url']
+    city_url_robots = city_url + 'robots.txt'
+    robotstxt = requests.get(city_url_robots)
 
-    def run(self):
-        self.validate_robots()
+    if robotstxt.status_code == 404:
+        cities[city]['can_crawling'] = False
 
-    def get_sourcecode(self):
-        """
-        Faz requisição do código fonte da página inicial do sítio eletrônico.
-        Se bem sucedido, guarda duas novas keys no dict da cidade correspondente:
-            - date_sourcecode: timestamp da requisição
-            - sourcecode: conteúdo da resposta da requisição
+    if robotstxt.status_code == 200:
+        cities[city]['has_robotstxt'] = True
 
-        :return: <Response>.content OR None
-        """
-        try:
-            self.sourcecode = requests.get(self.city['url'], timeout=30)
+        robotparser = urllib.robotparser.RobotFileParser()
+        robotparser.set_url(city_url_robots)
+        robotparser.read()
 
-            if self.sourcecode.status_code == 200:
-                self.city['date_sourcecode'] = dt.timestamp(dt.now())
-                # self.city['sourcecode'] = self.sourcecode.content
-                return self.sourcecode.content
+        if robotparser.can_fetch('*', city_url):
+            cities[city]['can_crawling'] = True
 
-            return None
 
-        except requests.exceptions.RequestException as error:
-            print(dt.timestamp(dt.now()), self.city['city_name'], error)
+# Se puder fazer o crawling, faz request do Código Fonte da home dos sites e guarda em nova key do dict cities
+# A respeito do tipo de resposta da requisição:
+# .text = identificação automatica da resposta
+# .content = resposta em binário
+# https://2.python-requests.org/en/master/user/quickstart/#response-content
+for city in cities:
+    cities[city]['sourcecode'] = None
 
-    def validate_robots(self):
-        """
-        Confere o arquivo robots.txt dos sites e verifica condições de Dissalow
-        adicionando duas keys novas ao dict da cidade correspondente:
-            - has_robotstxt (True/False): responde a pergunta se tem ou não o arquivo robots.txt
-            - can_crawling (True/False): responde a pergunta se pode ou não fazer crawling
-        """
+    if cities[city]['can_crawling']:
+        sourcecode = requests.get(cities[city]['url'])
 
-        try:
-            self.city['has_robotstxt'] = False
-            self.city['can_crawling'] = False
+        if sourcecode.status_code == 200:
+            cities[city]['sourcecode'] = sourcecode.content
 
-            city_url = self.city['url']
-            city_url_robots = city_url + 'robots.txt'
-
-            robotstxt = requests.get(city_url_robots, timeout=30)
-
-            if robotstxt.status_code == 404:
-                self.city['can_crawling'] = False
-
-            if robotstxt.status_code == 200:
-                self.city['has_robotstxt'] = True
-
-                robotparser = urllib.robotparser.RobotFileParser()
-                robotparser.set_url(city_url_robots)
-                robotparser.read()
-
-                if robotparser.can_fetch('*', city_url):
-                    self.city['can_crawling'] = True
-                    self.validate_recommendations()
-
-        except requests.exceptions.RequestException as error:
-            print(dt.timestamp(dt.now()), self.city['city_name'], error)
-
-    def validate_recommendations(self):
-        """
-        A partir do código fonte da página inicial do sítio eletrônico, valida as
-        recomendações listadas e guarda suas ocorrências na lista de ocorrências.
-        """
-        if self.sourcecode:
-            # rec01 = Recommendation01(city['url']).validarhtml()
+            # rec01 = Recommendation01(cities[city]['url']).validarhtml()
             # occurrences.add({'rec01': rec01})
 
-            rec20 = Recommendation20(self.sourcecode).avaliacao()
-            occurrences.add({self.city['_id']: rec20})
+            rec20 = Recommendation20(cities[city]['sourcecode']).avaliacao()
+            occurrences.add({'rec20': rec20})
 
-
-cities = csv_file_to_dict(CSV_FILE)
-print(cities)
-
-for c in cities:
-    validate = ValidateCity(cities[c])
-    validate.start()
-    validate.join()
-
-print(cities)
 
 # Exibe ocorrências
 occurrences.show()
