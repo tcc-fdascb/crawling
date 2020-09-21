dd1c3e4e8054c0897219bbf3faed886718045d8e
Flávio Dutra <flavio.adore@gmail.com>
2020-09-21 17:15:48 -0300

    Refatoração da Recomendação 1

--- crawling/recommendations/recommendation01.py
+++ crawling/recommendations/recommendation01.py
@@ -1,25 +1,73 @@
+from bs4 import BeautifulSoup
+from string import Template
+import re
 import requests
+from urllib.parse import quote, urlparse
+
+from ..occurrences.occurrences import Occurrences
+from ..occurrences.occurrence_interface import OccurrenceInterface
+
+WEBSERVICE_CSS = 'http://jigsaw.w3.org/css-validator/validator?uri=$url&profile=css3&warning=0&output=json'
+WEBSERVICE_HTML = 'http://validator.w3.org/nu/?doc=$url&out=json'
+
+
+def encode_url(t):
+    return quote(t, safe='')
 
 
 class Recommendation01:
     """
-        Recomendação 01: Validar Html e CSS do Sitio Eletrônico
-
-
+        Recomendação 01: Validar HTML e CSS do sitio eletrônico
+        - CSS: se o atributo "validity" for true, a folha de estilo é válida
+        - HTML: se não houver em "messages" o tipo "error", o documento é válido
     """
-    def __init__(self, content):
+
+    def __init__(self, content, url):
+        self.rec = 1
         self.content = content
+        self.url = url
+        self.occurrences = Occurrences()
 
+    def validar_css(self):
+        soup = BeautifulSoup(self.content, 'html.parser')
+        link_tags = soup.find_all('link')
 
-    def validarcss(self):
-        #htmlanalizer = requests.get('http://jigsaw.w3.org/css-validator/validator?uri='+str(self.content)+'&profile=css3svg&warning=0&output=soap12')
-        cssanalizer = requests.get('http://jigsaw.w3.org/css-validator/validator?uri=www.google.com.br&profile=css3svg&warning=0&output=text')
-        if cssanalizer.status_code == 200:
-            print(cssanalizer.headers)
-            print(cssanalizer.text)
+        for link_tag in link_tags:
+            href = link_tag.get('href')
 
-    def validarhtml(self):
-        htmlanalizer = requests.get('http://validator.w3.org/nu/?doc='+str(self.content)+'&out=text')
-        if htmlanalizer.status_code == 200:
-            print(htmlanalizer.text)
+            if self.url not in href:
+                href = self.url + href
 
+            if re.search('(app|main|styles?|global|estilos?|default).css', href) is not None \
+               and re.search('plugins?|libs?|bootstrap|materialize', href) is None:
+                url = Template(WEBSERVICE_CSS).substitute(url=encode_url(href))
+                analizer = requests.get(url)
+
+                if analizer.status_code == 200:
+                    res = analizer.json()
+                    if res['cssvalidation']['validity']:
+                        self.occurrences.add(OccurrenceInterface(self.rec, 2, link_tag))
+                    else:
+                        self.occurrences.add(OccurrenceInterface(self.rec, 3, link_tag))
+
+        return self.occurrences.list_of_occurrences
+
+    def validar_html(self):
+        is_valid = True
+        url = Template(WEBSERVICE_HTML).substitute(url=encode_url(self.url))
+        analizer = requests.get(url)
+
+        if analizer.status_code == 200:
+            res = analizer.json()
+
+            for msg in res['messages']:
+                if msg['type'] == 'error':
+                    is_valid = False
+                    break
+
+        if is_valid:
+            self.occurrences.add(OccurrenceInterface(self.rec, 0, ''))
+        else:
+            self.occurrences.add(OccurrenceInterface(self.rec, 1, ''))
+
+        return self.occurrences.list_of_occurrences
