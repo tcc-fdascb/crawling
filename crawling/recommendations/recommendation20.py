40e515f53f57109ea69f3e29dbf7ee02308ef288
Flávio Dutra <flavio.adore@gmail.com>
2020-09-24 18:01:18 -0300

    Rec20: Refatoração e implementação de novas checagens

--- crawling/recommendations/recommendation20.py
+++ crawling/recommendations/recommendation20.py
@@ -1,5 +1,4 @@
 from bs4 import BeautifulSoup
-import itertools
 
 from ..occurrences.occurrences import Occurrences
 from ..occurrences.occurrence_interface import OccurrenceInterface
@@ -10,11 +9,12 @@ class Recommendation20:
     Recomendação 20: Fornecer alternativa em texto para as imagens do sítio
 
     Verificações:
-        - Imagens com conteúdo sem descrição
-        - Imagens com descrição inadequada
-        - Imagens diferentes com a mesma descrição
+        - Imagens sem descrição (sem alt)
+        - Imagens decorativas
+        - Imagens com descrição longas
         - Imagem com dupla descrição, title e alt iguais
-        - Imagens com descrições longas no alt
+        - Imagens com descrição inadequada (nome do arquivo, “alt”, “descrição”, “imagem”, etc)
+        - Imagens diferentes com a mesma descrição
     """
 
     def __init__(self, sourcecode):
@@ -25,26 +25,46 @@ class Recommendation20:
     def avaliacao(self):
         soap = BeautifulSoup(self.sourcecode, 'html.parser')
         imagens = soap.find_all('img')
+        imagens_alt = []
 
         for imagem in imagens:
-            if not imagem.has_attr('alt') or not imagem['alt']:
+            has_error_or_warning = False
+
+            # Imagens com conteúdo sem descrição (sem alt)
+            if not imagem.has_attr('alt'):
                 self.occurrences.add(OccurrenceInterface(self.rec, 1, imagem))
+                has_error_or_warning = True
             else:
+                # Imagens decorativas
+                if imagem['alt'] == '':
+                    self.occurrences.add(OccurrenceInterface(self.rec, 2, imagem))
+                    has_error_or_warning = True
+
+                # Imagens com descrição longas
+                if len(imagem['alt']) > 140:
+                    self.occurrences.add(OccurrenceInterface(self.rec, 3, imagem))
+                    has_error_or_warning = True
+
+                # Imagem com dupla descrição, title e alt iguais
+                if 'title' in imagem.attrs:
+                    if imagem['alt'] == imagem['title']:
+                        self.occurrences.add(OccurrenceInterface(self.rec, 4, imagem))
+                        has_error_or_warning = True
+
+                # Imagens com descrição inadequada
+                for desc in ['figura', 'imagem', 'image', 'img', 'alt', 'descrição', 'desc', 'foto', 'photo', 'picture', 'pic']:
+                    if desc == imagem['alt']:
+                        self.occurrences.add(OccurrenceInterface(self.rec, 5, imagem))
+                        has_error_or_warning = True
+
+                # Imagens diferentes com a mesma descrição
+                imagens_alt.append(imagem['alt'])
+                if imagem['alt'] != '' and imagem['alt'] in imagens_alt[:-1]:
+                    self.occurrences.add(OccurrenceInterface(self.rec, 6, imagem))
+                    has_error_or_warning = True
+
+            # Não encontrou erros
+            if not has_error_or_warning:
                 self.occurrences.add(OccurrenceInterface(self.rec, 0, imagem))
-
-            if len(imagem['alt']) > 100:
-                self.occurrences.add(OccurrenceInterface(self.rec, 2, imagem))
-
-            for i in ["figura", "imagem", "alt", "descrição", "foto"]:
-                if i == imagem['alt']:
-                    self.occurrences.add(OccurrenceInterface(self.rec, 0, imagem))
-
-            if imagem['alt'] == imagem['title']:
-                self.occurrences.add(OccurrenceInterface(self.rec, 0, imagem))
-
-        for compara1, compara2 in itertools.combinations(imagens, 2):
-            if compara1['alt'] == compara2['alt'] and compara1['src'] != compara2['src']:
-                # self.occurrences.add(OccurrenceInterface(self.rec, 2, imagem))
-                pass
 
         return self.occurrences.list_of_occurrences
