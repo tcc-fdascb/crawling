8f9d9f1bf67800ee79ec5e243744c8d6b2d2951f
Flávio Dutra <flavio.adore@gmail.com>
2020-09-24 10:59:26 -0300

    Removido /tests

--- tests/dt.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime as dt
-
-a = int(dt.timestamp(dt.now()))
-print(a)
-
-b = dt.fromtimestamp(a)
-print(b)
--- tests/teste.py
+++
@@ -1,5 +0,0 @@
-import itertools
-import re
-texto = re.sub("\s", "", 'eu sou o o       este')
-
-print(texto)
