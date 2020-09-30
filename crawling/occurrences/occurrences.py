672d9efdbb527e3d318d55afd6985584c5405750
Flávio Dutra <flavio.adore@gmail.com>
2020-09-30 15:27:17 -0300

    Adicionado verificações de lista vazia

--- crawling/occurrences/occurrences.py
+++ crawling/occurrences/occurrences.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 def translate_message(messages, recommendation, code_message):
     code = ['OK', 'ERRO', 'ALERTA']
-
     for message in messages:
         if messages[message]['recommendation'] == recommendation and messages[message]['code_message'] == code_message:
             return code[messages[message]['type_feedback']] + ': ' + messages[message]['message']
@@ -13,7 +12,6 @@ class Occurrences:
     """
     Organiza as ocorrências em uma lista e trata sua exibição
     """
-
     def __init__(self):
         self.list_of_occurrences = []
 
@@ -23,12 +21,15 @@ class Occurrences:
     def show(self):
         messages = pd.read_csv('data/messages.csv').to_dict(orient='index')
 
-        print("Lista de ocorrências:")
-        for occurrence in self.list_of_occurrences:
-            for key in occurrence.keys():
-                print(key)
-                for value in occurrence.get(key):
-                    print(value.recommendation,
-                          value.code_message,
-                          translate_message(messages, value.recommendation, value.code_message),
-                          value.tag)
+        if len(self.list_of_occurrences) > 0 and self.list_of_occurrences[0][0] is not None:
+            print("Lista de ocorrências:")
+            for occurrence in self.list_of_occurrences:
+                for key in occurrence.keys():
+                    print(key)
+                    for value in occurrence.get(key):
+                        print(value.recommendation,
+                              value.code_message,
+                              translate_message(messages, value.recommendation, value.code_message),
+                              value.tag)
+        else:
+            print("Lista de ocorrências vazia.")
