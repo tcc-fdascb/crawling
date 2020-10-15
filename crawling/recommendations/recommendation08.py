54e4f685f23b431e97bc07df9b861c1795844ffb
AndreiSantanaK20 <63802539+AndreiSantanaK20@users.noreply.github.com>
2020-10-15 11:05:10 -0300

    Ajustes rec8

--- crawling/recommendations/recommendation08.py
+++ crawling/recommendations/recommendation08.py
@@ -17,7 +17,7 @@ class Recommendation08:
 
     def avaliacao(self):
         # ispassou = True
-        soap = BeautifulSoup("<p><a id=1 ></a> <br> <a id=2> </a></p>", 'html.parser')
+        soap = BeautifulSoup(self.sourcecode, 'html.parser')
         remove = soap.find_all(text=lambda text: isinstance(text, Comment))
         for removeitem in remove:  # remove o código html comentado
             removeitem.extract()
@@ -34,22 +34,22 @@ class Recommendation08:
                 if '</a><a' in par:  # verifica se tem um fechamento ligado na abertura de outra tag a
                     # dispara erro
                     ispassou = False
-                    self.occurrences.add(OccurrenceInterface(self.rec, 1, par, 1))
+                    self.occurrences.add(OccurrenceInterface(self.rec, 1, par, 2))
                 if '</a> <a' in par:  # verifica se tem espaço entre fechamento e abertura de outra tag a
                     # dispara erro
-                    self.occurrences.add(OccurrenceInterface(self.rec, 1, par, 1))
+                    self.occurrences.add(OccurrenceInterface(self.rec, 1, par, 2))
                     ispassou = False
                 if conj[0] in paragrafosemespaco or conj[1] in paragrafosemespaco or conj[2] in paragrafosemespaco:
                     # verifica se tem a tag de quebra de linha entre fechamento e abertura de outra tag a
                     # dispara erro
-                    self.occurrences.add(OccurrenceInterface(self.rec, 1, par, 1))
+                    self.occurrences.add(OccurrenceInterface(self.rec, 1, par, 2))
                     ispassou = False
                 if str("</a>\n<a") in paragrafosemespaco:
                     # verifica se tem quebra de linha entra o fechamento e abertura de outra tag a
-                    self.occurrences.add(OccurrenceInterface(self.rec, 1, par, 1))
+                    self.occurrences.add(OccurrenceInterface(self.rec, 1, par, 2))
                     ispassou = False
                 if "</a><br>\n<a" in paragrafosemespaco:
-                    self.occurrences.add(OccurrenceInterface(self.rec, 1, par, 1))
+                    self.occurrences.add(OccurrenceInterface(self.rec, 1, par, 2))
                     ispassou = False
 
                 if ispassou:
