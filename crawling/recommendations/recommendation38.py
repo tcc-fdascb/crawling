573ff1df57816523e91c1912ba7f183f08bc3205
Flávio Dutra <flavio.adore@gmail.com>
2020-10-20 16:17:52 -0300

    Rec38: QA

--- crawling/recommendations/recommendation38.py
+++ crawling/recommendations/recommendation38.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup , Comment
+from bs4 import BeautifulSoup
 
 from ..occurrences.occurrences import Occurrences
 from ..occurrences.occurrence_interface import OccurrenceInterface
@@ -16,36 +16,29 @@ class Recommendation38:
 
     def avaliacao(self):
         soap = BeautifulSoup(self.sourcecode, 'html.parser')
-        remove = soap.find_all(text=lambda text:isinstance(text, Comment))
 
-        ispassou = True
-        # remove o código html comentado
-        for removeitem in remove:
-            removeitem.extract()
-        soapfiltro = BeautifulSoup(soap.prettify(), 'html.parser')
+        inputs = soap.select('input')
 
-        for inputverifica in soapfiltro.select('input'):
-             if inputverifica['type'] == 'image':
-                 if not inputverifica['alt']:
-                    self.occurrences.add(OccurrenceInterface(self.rec, 1, inputverifica, 3))
+        if len(inputs) > 0:
+            for input_tag in inputs:
+                ispassou = True
+                attr_type = input_tag.get('type')
+
+                if attr_type:
+                    if attr_type == 'image':
+                        if not input_tag.get('alt'):
+                            ispassou = False
+                            self.occurrences.add(OccurrenceInterface(self.rec, 1, input_tag, 3))
+
+                    if attr_type == 'reset' or attr_type == 'button' or attr_type == 'submit':
+                        if not input_tag.get('value'):
+                            ispassou = False
+                            self.occurrences.add(OccurrenceInterface(self.rec, 1, input_tag, 3))
+                else:
                     ispassou = False
+                    self.occurrences.add(OccurrenceInterface(self.rec, 2, input_tag, 3))
 
-             if inputverifica['type'] == 'reset':
-                 if not inputverifica['value']:
-                    self.occurrences.add(OccurrenceInterface(self.rec, 1, inputverifica, 3))
-                    ispassou = False
-
-             if inputverifica['type'] == 'button':
-                 if not inputverifica['value']:
-                    self.occurrences.add(OccurrenceInterface(self.rec, 1, inputverifica, 3))
-                    ispassou = False
-
-             if inputverifica['type'] == 'submit':
-                 if not inputverifica['value']:
-                    self.occurrences.add(OccurrenceInterface(self.rec, 1, inputverifica, 3))
-                    ispassou = False
-
-        if ispassou:
-            self.occurrences.add(OccurrenceInterface(self.rec, 0, "", 3))
+                if ispassou:
+                    self.occurrences.add(OccurrenceInterface(self.rec, 0, input_tag, 3))
 
         return self.occurrences.list_of_occurrences
