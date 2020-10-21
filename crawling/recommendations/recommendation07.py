02cc8c7e5e02afb18975dc3c0461a1576f5abf27
Flávio Dutra <flavio.adore@gmail.com>
2020-10-21 17:40:55 -0300

    Recs 7, 8 e 38: QA

--- crawling/recommendations/recommendation07.py
+++ crawling/recommendations/recommendation07.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup, Comment
+from bs4 import BeautifulSoup
 
 from ..occurrences.occurrences import Occurrences
 from ..occurrences.occurrence_interface import OccurrenceInterface
@@ -16,35 +16,48 @@ class Recommendation07:
 
     def avaliacao(self):
         soap = BeautifulSoup(self.sourcecode, 'html.parser')
-        remove = soap.find_all(text=lambda text: isinstance(text, Comment))
-        ispassou = True
-        for removeitem in remove:  # remove o código html comentado
-            removeitem.extract()
+        tables = soap.select('table')
 
-        # tables = soap.select('table form')
-        if len(soap.find_all('body > iframe')) > 0 and len(soap.find_all('body > div')) == 0 and len(
-                soap.find_all('body > main')) == 0:
-            # dispara um warning dizendo algo como: "Possivelmente o conteúdo da página está em outro endereço."
-            ispassou = False
-            self.occurrences.add(OccurrenceInterface(self.rec, 3,""))
+        for table in tables:
+            ispassou = True
+            words = ['topo', 'main', 'rodape', 'principal', 'menu', 'nav',
+                     'navigation', 'navegacao', 'header', 'footer']
 
-        for tableitem in soap.select('table form'):
-            if tableitem:
-                self.occurrences.add(OccurrenceInterface(self.rec, 2, tableitem, 2))
+            if table.get('id'):
+                check = any(item in table.get('id') for item in words)
+                if check:
+                    self.occurrences.add(OccurrenceInterface(self.rec, 1, table, 2))
+                    ispassou = False
+
+            if table.get('class'):
+                check = any(item in table.get('class') for item in words)
+                if check:
+                    self.occurrences.add(OccurrenceInterface(self.rec, 1, table, 2))
+                    ispassou = False
+
+            for word in words:
+                childclass = table.select('.' + word)
+                childid = table.select('#' + word)
+
+                if childclass:
+                    self.occurrences.add(OccurrenceInterface(self.rec, 1, childclass, 2))
+                    ispassou = False
+
+                if childid:
+                    self.occurrences.add(OccurrenceInterface(self.rec, 1, childclass, 2))
+                    ispassou = False
+
+            if len(table.select('form')) > 0:
+                self.occurrences.add(OccurrenceInterface(self.rec, 2, table, 2))
                 ispassou = False
 
-        for verifica in ['topo', 'main', 'rodape', 'principal', 'menu', 'nav', 'navigation', 'navegacao', 'header',
-                         'footer']:
-            selectionclass = 'table .' + verifica
-            selectionids = 'table #' + verifica
-            if soap.select(selectionclass):
-                self.occurrences.add(OccurrenceInterface(self.rec, 1, soap.select(selectionclass), 2))
-                ispassou = False
-            if soap.select(selectionids):
-                self.occurrences.add(OccurrenceInterface(self.rec, 1, soap.select(selectionids), 2))
-                ispassou = False
+            if ispassou:
+                pass
+                self.occurrences.add(OccurrenceInterface(self.rec, 0, "", 2))
 
-        if ispassou:
-            self.occurrences.add(OccurrenceInterface(self.rec, 0,"", 2))
+        if len(soap.select('body > iframe')) > 0 and\
+                len(soap.select('body > div')) == 0 and\
+                len(soap.select('body > main')) == 0:
+            self.occurrences.add(OccurrenceInterface(self.rec, 3, "", 2))
 
         return self.occurrences.list_of_occurrences
