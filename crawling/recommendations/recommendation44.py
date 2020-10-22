cddc7490419aa62155c9b336229215d010d91e7a
Flávio Dutra <flavio.adore@gmail.com>
2020-10-22 11:38:22 -0300

    Rec44: QA

--- crawling/recommendations/recommendation44.py
+++ crawling/recommendations/recommendation44.py
@@ -1,12 +1,12 @@
-from bs4 import BeautifulSoup, Comment
+from bs4 import BeautifulSoup
 
 from ..occurrences.occurrences import Occurrences
 from ..occurrences.occurrence_interface import OccurrenceInterface
 
+
 class Recommendation44:
     """
-        Recomendação 44: Utilizar corretamente os níveis de cabeçalho
-
+        Recomendação 44: Agrupar campos de formulário
     """
     def __init__(self, sourcecode):
         self.rec = 44
@@ -14,35 +14,35 @@ class Recommendation44:
         self.occurrences = Occurrences()
 
     def avaliacao(self):
+        soap = BeautifulSoup(self.sourcecode, 'html.parser')
+        forms = soap.find_all('form')
+        selects = soap.select('select')
 
-        soap = BeautifulSoup(self.sourcecode)
-        remove = soap.find_all(text=lambda text: isinstance(text, Comment))
-        ispassou = True
-        for removeitem in remove:  # remove o código html comentado
-            removeitem.extract()
-            Fieldsetcoll = soap.select('form fieldset')
+        if forms:
+            for form in forms:
+                has_fieldset = True
+                has_legend = True
+                fieldsets = form.find_all('fieldset')
 
-        if not Fieldsetcoll:
-            self.occurrences.add(OccurrenceInterface(self.rec, 1, "", 1))
-            ispassou = False
-        else:
-            for item in Fieldsetcoll:
-                if not item.findAll('legend'):
-                    self.occurrences.add(OccurrenceInterface(self.rec, 2, item, 1))
-                    ispassou = False
+                if not fieldsets:
+                    has_fieldset = False
+                    self.occurrences.add(OccurrenceInterface(self.rec, 2, form, 2))
+                else:
+                    for fieldset in fieldsets:
+                        if not fieldset.find('legend'):
+                            has_legend = False
+                            self.occurrences.add(OccurrenceInterface(self.rec, 3, form, 2))
+                            break
 
-        for item in soap.select('select'):
-            optgroupcoll = item.findAll('optgroup')
-            if not optgroupcoll:
-                self.occurrences.add(OccurrenceInterface(self.rec, 3, item, 1))
-                ispassou = False
-            else:
-                for optgroup in optgroupcoll:
-                    if not optgroup['label']:
-                        self.occurrences.add(OccurrenceInterface(self.rec, 4, optgroup, 1))
-                        ispassou = False
+                if has_fieldset and has_legend:
+                    self.occurrences.add(OccurrenceInterface(self.rec, 0, form, 2))
 
-        if ispassou:
-            self.occurrences.add(OccurrenceInterface(self.rec, 0, "", 1))
+        if selects:
+            for select in selects:
+                options = select.find_all('option')
+                if len(options) > 5 and select.find('optgroup'):
+                    self.occurrences.add(OccurrenceInterface(self.rec, 4, select, 2))
+                else:
+                    self.occurrences.add(OccurrenceInterface(self.rec, 1, select, 2))
 
         return self.occurrences.list_of_occurrences
